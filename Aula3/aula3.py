8ad26a837477e8a13606731bc5a7df3222dd7e43
NatanCDalmarco <tan_uy@hotmail.com>
2019-11-19 09:11:13 -0300

    TrabalhosPython

--- Aula3/aula3.py
+++ Aula3/aula3.py
@@ -1,5 +1,5 @@
-numero1 = 10
-numero2 = 15
+numero1 = 5839.45
+numero2 = 0.11
 
 print('='*50, '\n')
 resultado = numero1 + numero2
@@ -13,4 +13,18 @@ print(resultado)
 
 resultado = numero1 / numero2
 print(f'{resultado:.2f}')
+
+resultado = numero1 // numero2
+print(resultado)
+
+resultado = numero1 % numero2
+print(resultado)
+
+resultado = numero1 ** numero2
+print(resultado)
+
+resultado = numero1 ** (1/numero2)
+print(resultado)
+
+
 print('\n', '='*50) 
---
+++ Aula9/aula9-Exercicio(3).py
@@ -0,0 +1,32 @@
+pirnt('='*50, '\n')
+
+salario = float(input('Digite seu salario:'))
+# cargo = (input('Digite seu cargo:'))
+# num_registro = int(input('Digite seu registro:'))
+# cpf = (input('Digite seu cpf:'))
+# nome_completo = (input('Digite nome completo:'))
+
+# if (salario > 0 and salario <= 1751.81):
+#     inss = salario * 0.08
+# elif (salario > 1751.81 and salario <= 2919.72):
+#     inss = salario * 0.095
+# elif (salario > 2919.72 and salario <= 5839.45):
+#     inss = salario * 0.11
+# else:
+#     inss = 642.3395
+
+# irff = 0
+# if (salario > 1903.98 and salario <= 2826.65):
+#     irrf = (salario  - inss)* 0.075
+# elif (salario > 2826.65 and salario <= 3751.05):
+#     irrf = (salario  - inss) * 0.15
+# elif (salario > 3751.05 and salario <= 4664.68):
+#     irrf = (salario  - inss) * 0.225
+# elif (salario > 4664.68):
+#     irrf = (salario  - inss) * 0.275
+
+# salario_liquido = salario - fgts - inss - irff
+
+# print(f'Seu saláro líquido é {salario_liquido}')
+
+# pirnt('\n', '='*50)
