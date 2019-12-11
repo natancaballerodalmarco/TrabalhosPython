241ccf228f3105781164cd666e86f8dc918e74c0
natacaballero <tan_uy@hotmail.com>
2019-12-11 11:22:38 -0300

    Exercício 22

--- 22-Aula22/exercicio/exercicio1a.py
+++ 22-Aula22/exercicio/exercicio1a.py
@@ -14,34 +14,48 @@
 
 class Pessoa:
     
-    def __init__(self, codigo, cpf, nome, sexo):
-        self.codigo = codigo
-        self.cpf = cpf
-        self.nome = nome
-        self.sexo = sexo
-        self.carteira = 
-        self.divida = 
-        self.bens = 
+    def __init__(self):
+        self.codigo = 1
+        self.cpf = 12345678989
+        self.nome = 'Jorje'
+        self.sexo = 'M'
+        self.carteira = 0
+        self.divida = 0
+        self.bens = 0
     
     def saldo (self, salario, compra, bens):
-        self.carteira = self.carteira + salario
-        self.divida = self.divida + compra
-        self.bens = self.bens + bens
+        self.carteira += salario
+        self.divida += compra
+        self.bens += bens
 
         if self.divida < self.carteira:
             self.carteira = self.carteira - self.divida
             self.divida = 0
+            print(f'Você possui {self.carteira} reais na carteira')
+            print(f'Você deve {self.divida} reais')
             print(f'Você possui {self.bens} bens')
+
+        elif self.divida > self.carteira:
+            self.divida = self.divida - self.carteira
+            self.carteira = 0
+            print(f'Você possui {self.carteira} reais na carteira')
+            print(f'Você deve {self.divida} reais')
+            print(f'Você possui {self.bens} bens')
+
         else:
+            print(f'Você possui {self.carteira} reais na carteira')
+            print(f'Você deve {self.divida} reais')
             print(f'Você possui {self.bens} bens')
 
 
-
-p = Pessoa(0, 89452315789, 'Natan', 'M')
-salario = float(input('Digite o salário: '))
-compra = float(input('Digite o valor gasto em compras: '))
-bens = float(input('Digite a quantidade de coisas compradas: '))
-p.saldo(salario, compra, bens)
+a = 'b'
+p = Pessoa()
+while a != 'a':
+    a = input('Se você deseja parar, digite "a": ')
+    salario = float(input('Digite o salário: '))
+    compra = float(input('Digite o valor gasto em compras: '))
+    bens = int(input('Digite a quantidade de coisas compradas: '))
+    p.saldo(salario, compra, bens)
 
 print(p.codigo)
 print(p.cpf)
